d771575178cdde5b11bc19bb38ddd6561e924ac5
Zahari <zahari.cheyreckov999@gmail.com>
2024-01-12 15:12:22 +0200

    Configure movies data array

---
+++ data/movies.js
@@ -0,0 +1,76 @@
+const Movies = [
+    {
+        id: 1,
+        name: "Interstellar",
+        year: 2014,
+        watched: true,
+        rating: 8.6
+    },
+    {
+        id: 2,
+        name: "Prometheus",
+        year: 2012,
+        watched: true,
+        rating: 7.0
+    },
+    {
+        id: 3,
+        name: "Oppenheimer",
+        year: 2023,
+        watched: true,
+        rating: 7.2
+    },
+    {
+        id: 4,
+        name: "The Martian",
+        year: 2015,
+        watched: true,
+        rating: 7.5
+    },
+    {
+        id: 5,
+        name: "Whipwash",
+        year: 2016,
+        watched: true,
+        rating: 6.3
+    },
+    {
+        id: 6,
+        name: "21",
+        year: 2008,
+        watched: true,
+        rating: 7.7
+    },
+    {
+        id: 7,
+        name: "Inception",
+        year: 2010,
+        watched: true,
+        rating: 8.8
+    },
+    {
+        id: 8,
+        name: "Memento",
+        year: 2000,
+        watched: true,
+        rating: 8.5
+    },
+    {
+        id: 9,
+        name: "Limitless",
+        year: 2011,
+        watched: true,
+        rating: 6.8
+    },
+    {
+        id: 10,
+        name: "Circle",
+        year: 2015,
+        watched: true,
+        rating: 6.2
+    },
+];
+
+module.exports = {
+    Movies: Movies
+};
