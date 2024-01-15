bf6a021fbefcc72e128362d49f1336d7e1b7f275
Zahari <zahari.cheyreckov999@gmail.com>
2024-01-15 13:47:49 +0200

    Configure apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import Movies from "./components/Movies";
+import Users from "./components/Users";
+
 import './App.css';
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:4000/graphql"
+  });
+
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Movies />
+        <Users />
+      </div>
+    </ApolloProvider>
   );
 }
 
